7ce324116c36e7dbf9fdbd562f8e927227c6e008
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-08 02:32:37 -0500

    array helper

---
+++ array-helper2.js
@@ -0,0 +1,15 @@
+//create an array of numbers
+var numbers = [1,2,3,4,5];
+
+//Create a variable to hold the sum
+var sum = 0;
+
+function adder(number) {
+   sum += number; 
+}
+
+//Loop over array, incrementing the sum variable 
+numbers.forEach(adder);
+
+//Print the sum variable
+console.log(sum);
